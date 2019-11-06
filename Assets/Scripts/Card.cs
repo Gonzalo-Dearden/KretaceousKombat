5f240d58fce226f5d93f388ee4f8e4af5dfefe0e
MATTHEW SHAW <MS228668@falmouth.ac.uk>
2019-11-06 10:26:24 +0000

    Created abstract card class and started work on card displayer

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Card : MonoBehaviour
+public abstract class Card : MonoBehaviour
 {
     public DamageType damageType;
     public string Description;
     public Sprite DisplayImage;
 
-    public void Start()
-    {
-        GetComponentInChildren<Image>().sprite = DisplayImage;
-    }
+    public abstract void Start();
+    public abstract void ApplyEffect();
 }
---
+++ Assets/Scripts/DoubleDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleDamage : Card
+{
+    public override void Start()
+    {
+        damageType = DamageType.All;
+        Description = "Doubles the player damage for the rest of the match";
+    }
+
+    public override void ApplyEffect()
+    {
+        GameObject.FindGameObjectWithTag("BattleManager").GetComponent<Battle>().PlayerDamage = 10;
+    }
+
+    
+}
