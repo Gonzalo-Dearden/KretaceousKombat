789b0f46d78ee4976c2a0b92ec499da9c3464592
MATTHEW SHAW <matthew.w.shaw74@gmail.com>
2019-11-18 22:06:21 +0000

    Added ability to see what the damn cards are before you play them

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
 public enum Player { Player, Enemy};
 
@@ -21,6 +23,18 @@ public abstract class Card : MonoBehaviour
     public void Start()
     {
         GetComponent<Button>().image.sprite = DisplayImage;
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerEnter;
+        entry.callback.AddListener((data) => { MouseOn(); });
+        trigger.triggers.Add(entry);
+
+        EventTrigger.Entry exit = new EventTrigger.Entry();
+        exit.eventID = EventTriggerType.PointerExit;
+        exit.callback.AddListener((data) => { MouseOff(); });
+        trigger.triggers.Add(exit);
+
+        GetComponentInChildren<TextMeshProUGUI>().text = Title + "\n" + Description;
     }
 
     public void Choose()
@@ -33,5 +47,17 @@ public abstract class Card : MonoBehaviour
         GetComponent<Button>().interactable = false;
         GameObject.FindGameObjectWithTag("BattleManager").GetComponent<Battle>().NextPlayerCard = this;
         GameObject.FindGameObjectWithTag("BattleManager").GetComponent<Battle>().ProceedButton.interactable = true;
+    }
+
+    public void MouseOn()
+    {
+        transform.localScale = new Vector3(4, 4, 4);
+        transform.SetAsLastSibling();
+        Debug.Log(transform.position);
+    }
+
+    public void MouseOff()
+    {
+        transform.localScale = new Vector3(2, 2, 2);
     }
 }
